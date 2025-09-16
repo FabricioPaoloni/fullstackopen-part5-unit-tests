6662bf3799a85b7913dd7c28c77c0e16e6b9f392
Fabricio Paoloni <paolonifabricio@gmail.com>
2025-09-16 13:58:56 -0300

    Exercise 5.7 - COMPLETED -

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [ showMore, setShowMore ] = useState(false)
+  const toggleVisibility = () => setShowMore(!showMore)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+
+  return(
+  <div style={showMore ? blogStyle: {} }>
+    {blog.title} by {blog.author} - <button onClick={toggleVisibility}>{ showMore ? 'hide' : 'view' }</button>
+    {showMore && <div>
+      <p>url: {blog.url}</p>
+      <p>likes: {blog.likes} <button>like</button></p>
+      <p>user: {blog.user.name}</p>
+    </div>}
   </div>  
-)
+)}
 
 export default Blog
